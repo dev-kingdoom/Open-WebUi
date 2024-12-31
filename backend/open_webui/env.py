415341cd42c91b1dc1c6635b953f4eade6172122
dev-kingdoom <mubarik1234@gmail.com>
2024-12-31 12:10:25 +0100

    Update env.py

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -101,7 +101,7 @@ log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "2Pac AI")
 if WEBUI_NAME != "2Pac AI":
-    WEBUI_NAME += " 2Pac AI"
+    WEBUI_NAME += " (2Pac AI)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
